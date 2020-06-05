77ab3a9502752d864ffb6da0e05b016b9cc1a54f
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2020-06-05 19:09:45 +0200

    tcp: send files in chunks

--- src/goophi/tcp.clj
+++ src/goophi/tcp.clj
@@ -3,28 +3,37 @@
             [aleph.tcp :as tcp]
             [clojure.core.async :as async]
             [goophi.core :as core]
-            [goophi.fs :as fs])
+            [goophi.fs :as fs]
+            [goophi.config :as config])
   (:import [java.io InputStream ByteArrayInputStream]))
 
-(def ^:private base-path "/home/sf/tmp/gopher")
-
-(defn- ->stream
+(defn- response->stream
   [val]
   (cond
     (instance? InputStream val) val
-    (instance? String val) (->stream (.getBytes val))
-    (bytes? val) (ByteArrayInputStream. val)
-    :else nil))
+    (instance? String val) (response->stream (.getBytes val))
+    (core/Item? val) (response->stream (str val))
+    (bytes? val) (ByteArrayInputStream. val)))
 
-(defn- process-request
+(defn- put-response!
+  [out response]
+  (when response
+    (with-open [stream (response->stream response)]
+      (loop [buffer (byte-array 8192)]
+        (let [available (.read stream buffer)]
+          (when (>= available 0)
+            @(s/put! out (byte-array (take available buffer)))
+            (recur buffer)))))))
+
+(defn- apply-handler
   [data]
-  (let [request (String. (byte-array data))]
-    (try
-      (if-let [[selector query] (core/parse-request request)]
-        (let [response (fs/get-contents base-path selector)]
-          (->stream response))
-        "Bad Request.")
-      (catch Exception e (str "Internal Server Error: " (.getMessage e))))))
+  (config/bind [^:required document-path [:runtime :document-path]]
+    (let [request (String. (byte-array data))]
+      (try
+        (if-let [[selector query] (core/parse-request request)]
+          (fs/get-contents document-path selector)
+          (core/error "Bad Request."))
+        (catch Exception e (core/error (str "Internal Server Error: " (.getMessage e))))))))
 
 (defn- split-bytes
   [data separator]
@@ -36,17 +45,17 @@
 
 (defn- handle-request
   [in out]
-  (async/go
+  (future
     (loop [buffer nil]
-      (when-let [data (async/alt! [in (async/timeout 5000)] ([v _] v))]
+      (when-let [data (async/alt!! [in (async/timeout 5000)] ([v _] v))]
         (let [[l r] (split-bytes data \newline)
               buffer' (concat buffer l)]
           (if (exceeds-maximum? buffer')
             (s/put! out "Request too long.\r\n")
             (if-not (empty? r)
               (->> (drop-last l)
-                   process-request
-                   (s/put! out))
+                   apply-handler
+                   (put-response! out))
               (recur buffer'))))))
     (.close out)))
 
