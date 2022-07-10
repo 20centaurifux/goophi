fa00f7b6f409ebd9f09d0b7992a7cc050cd00c89
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2022-07-10 15:54:34 +0200

    updated project dependencies; set version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,21 +1,20 @@
-(defproject zcfux/goophi "0.1.0"
+(defproject zcfux/goophi "0.1.1"
   :description "Gopher protocol library."
   :url "https://github.com/20centaurifux/goophi"
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [instaparse "1.4.10"]
-                 [org.clojure/core.async "1.3.610"]
-                 [org.clojure/core.memoize "1.0.236"]
+                 [instaparse "1.4.12"]
+                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/core.memoize "1.0.257"]
                  [hiccup "1.0.5"]
-                 [zcfux/confick "0.1.1"]
-                 [manifold "0.1.9-alpha4"]
-                 [org.clojure/core.async "1.3.610"]]
+                 [zcfux/confick "0.1.2"]
+                 [manifold "0.2.4"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :test {:dependencies [["aleph" "0.4.6"]]}}
-  :plugins [[lein-cljfmt "0.6.7"]
+  :plugins [[lein-cljfmt "0.8.2"]
             [lein-marginalia "0.9.1"]]
   :cljfmt {:indents {bind [[:inner 0]]}})
