06e74e50cfce0d6d3e649fd5f665c3092226f90f
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2024-11-27 21:29:50 +0100

    tcp: use ByteArrayOutputStream

--- src/clojure/goophi/tcp.clj
+++ src/clojure/goophi/tcp.clj
@@ -1,7 +1,7 @@
 (ns goophi.tcp
   (:require [clojure.string :as str]
-            [goophi.core :as goo]
-            [goophi.response :as response :as rsp]
+            [goophi.core :refer [info]]
+            [goophi.response :refer [menu-entity]]
             [manifold.deferred :as d]
             [manifold.stream :as s]))
 
@@ -9,18 +9,21 @@
 (defonce ^:private timeout-millis 5000)
 (defonce ^:private buffer-size 8192)
 
-(defn- ->path
-  [data]
-  (let [text (String. (byte-array data))]
-    (str/trimr text)))
+(defn- try-close!
+  [s]
+  (try
+    (s/close! s)
+    (catch Exception _)))
 
-(defn- split-bytes
-  [data separator]
-  (split-with #(not= % (byte separator)) data))
-
-(defn- exceeds-maximum?
-  [data]
-  (> (count data) max-request))
+(defn- put-response!
+  [s stream]
+  (let [buffer (byte-array buffer-size)]
+    (-> (d/loop []
+          (let [available (.read stream buffer)]
+            (when (pos? available)
+              (s/put! s (byte-array available buffer))
+              (d/recur))))
+        (d/finally #(try-close! stream)))))
 
 (defn- execute-handler
   [handler req]
@@ -28,44 +31,32 @@
     (let [response (handler req)]
       (if response
         response
-        (rsp/menu-entity (goo/info "Not found."))))
-    (catch Exception _ (rsp/menu-entity (goo/info "Internal server error.")))))
-
-(defn- try-close!
-  [s]
-  (try
-    (s/close! s)
-    (catch Exception _)))
-
-(defn- put-stream!
-  [s stream]
-  (let [buffer (byte-array buffer-size)]
-    (d/chain
-     (d/loop []
-       (let [available (.read stream buffer)]
-         (when (pos? available)
-           (s/put! s (byte-array available buffer))
-           (d/recur))))
-     (fn [_]
-       (try-close! s)
-       (try-close! stream)))))
+        (menu-entity (info "Not found."))))
+    (catch Exception _ (menu-entity (info "Internal server error.")))))
 
 (defn aleph-handler
   "Creates an Aleph handler."
   [handler]
-  (fn [s info]
-    (let [request (select-keys info [:remote-addr])]
-      (d/loop [buffer []]
-        (d/let-flow [data (s/try-take! s nil timeout-millis ::timeout)]
-          (if (#{::timeout} data)
-            (put-stream! s (rsp/menu-entity (goo/info "Connection timeout.")))
-            (when data
-              (let [[l r] (split-bytes data \newline)
-                    buffer' (concat buffer l)]
-                (if (exceeds-maximum? buffer')
-                  (put-stream! s (rsp/menu-entity (goo/info "Request too long.")))
-                  (if (seq r)
-                    (put-stream! s (execute-handler handler
-                                                    (assoc request
-                                                           :path (->path buffer'))))
-                    (d/recur buffer')))))))))))
+  (fn [s inf]
+    (let [request (select-keys inf [:remote-addr])
+          buffer (java.io.ByteArrayOutputStream.)]
+      (-> (d/loop []
+            (d/let-flow [data (s/try-take! s nil timeout-millis ::timeout)]
+              (if (#{::timeout} data)
+                (put-response! s (menu-entity (info "Connection timeout.")))
+                (when data
+                  (.write buffer data)
+                  (cond
+                    (> (.size buffer) max-request)
+                    (put-response! s (menu-entity (info "Request too long.")))
+
+                    (= (aget data (dec (alength data))) 10)
+                    (->> (.toString buffer "US-ASCII")
+                         str/trimr
+                         (assoc request :path)
+                         (execute-handler handler)
+                         (put-response! s))
+
+                    :else
+                    (d/recur))))))
+          (d/finally #(try-close! s))))))
